c450a5f6403c8abaf4b551c148e9458fe693008b
Aflynn50 <alastairflynn@hotmail.co.uk>
2019-03-21 10:57:01 +0000

    Fixing ssh honeypot 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Project: create a load of low interation honeypots to find a new attack, leave up for a while and find bugs
+
 from twisted.internet import reactor
 from twisted.logger import Logger
 from ssh import SSHpot
--- ssh.py
+++ ssh.py
@@ -1,16 +1,18 @@
+# https://stackoverflow.com/questions/36758571/python-ssh-servertwisted-conch-get-the-users-commands
+from twisted.python import log
 from twisted.conch.ssh.factory import SSHFactory
 from twisted.conch.ssh.keys import Key
 from twisted.cred.portal import Portal
 from twisted.internet import reactor
-from twisted.python.logfile import DailyLogFile
+import sys
 class SSHpot():
 
     def run(self):
-        with open('key.txt') as privateBlobFile:
+        with open('ssh_key') as privateBlobFile:
             privateBlob = privateBlobFile.read()
             privateKey = Key.fromString(data=privateBlob)
 
-        with open('key.txt.pub') as publicBlobFile:
+        with open('ssh_key.pub') as publicBlobFile:
             publicBlob = publicBlobFile.read()
             publicKey = Key.fromString(data=publicBlob)
 
@@ -18,5 +20,5 @@ class SSHpot():
         factory.privateKeys = {'ssh-rsa': privateKey}
         factory.publicKeys = {'ssh-rsa': publicKey}
         factory.portal = Portal(None)
-
+        log.startLogging(sys.stdout)
         reactor.listenTCP(2222, factory)
