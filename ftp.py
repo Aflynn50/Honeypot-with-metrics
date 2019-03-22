a0de93a2e1a37a9b7abee5c8707c5c4f2bb82f35
Aflynn50 <alastairflynn@hotmail.co.uk>
2019-03-22 13:46:27 +0000

    fixing ftp

--- ftp.py
+++ ftp.py
@@ -2,16 +2,12 @@ def ftp(insock, address):
     user = ""
     password = ""
     insock.sendall("220 (vsFTPd 3.0.3)\n".encode())
-    command = str(insock.recv(1024))
-    print(command)
+    command = str(insock.recv(1024))[2:-5]
     if command.startswith('USER'):
-
         user = command
     insock.sendall('331 Please specify the password.\n'.encode())
-    command = str(insock.recv(1024))
-    print(command)
+    command = str(insock.recv(1024))[2:-5]
     if command.startswith('PASS'):
-
         password = command
     insock.sendall('230 Login successful.\n'.encode())
     return user, password
