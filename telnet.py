87b3ac0deac3c5e9e00284a16a58e3767c9354a9
Aflynn50 <alastairflynn@hotmail.co.uk>
2019-03-22 11:26:13 +0000

    str

--- telnet.py
+++ telnet.py
@@ -1,7 +1,7 @@
 def telnet(insock, address):
     insock.send("Welcome to my fake Telnet server\n\nlogin: ".encode())
-    user = insock.recv(1024)[2:].strip()
+    user = str(insock.recv(1024))[2:].strip()
     insock.send("password: ".encode())
-    password = insock.recv(1024)[2:].strip()
+    password = str(insock.recv(1024))[2:].strip()
     insock.send("".encode())
     return user, password
