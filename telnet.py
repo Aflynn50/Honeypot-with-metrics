bfd54b28db27a7f973f79366610669fac59a1680
Aflynn50 <alastairflynn@hotmail.co.uk>
2019-03-22 11:23:18 +0000

    Fixing telnet

--- telnet.py
+++ telnet.py
@@ -1,6 +1,7 @@
 def telnet(insock, address):
     insock.send("Welcome to my fake Telnet server\n\nlogin: ".encode())
-    user = insock.recv(1024)
+    user = insock.recv(1024)[2:].strip()
     insock.send("password: ".encode())
-    password = insock.recv(1024)
+    password = insock.recv(1024)[2:].strip()
+    insock.send("".encode())
     return user, password
