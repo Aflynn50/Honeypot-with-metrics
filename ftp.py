02d692dbecc5b7f4e20bd99ca2eff5d2963ba50d
Aflynn50 <alastairflynn@hotmail.co.uk>
2019-03-22 16:21:06 +0000

    fixed again

--- ftp.py
+++ ftp.py
@@ -1,13 +1,16 @@
 def ftp(insock, address):
-    user = ""
-    password = ""
-    insock.sendall("220 (vsFTPd 3.0.3)\n".encode())
-    command = str(insock.recv(1024))[2:-5]
-    if command.startswith('USER'):
-        user = command[5:]
-    insock.sendall('331 Please specify the password.\n'.encode())
-    command = str(insock.recv(1024))[2:-5]
-    if command.startswith('PASS'):
-        password = command[5:]
-    insock.sendall('230 Login successful.\n'.encode())
-    return user, password
+    try:
+        user = ""
+        password = ""
+        insock.sendall("220 (vsFTPd 3.0.3)\n".encode())
+        command = str(insock.recv(1024))[2:-5]
+        if command.startswith('USER'):
+            user = command[5:]
+        insock.sendall('331 Please specify the password.\n'.encode())
+        command = str(insock.recv(1024))[2:-5]
+        if command.startswith('PASS'):
+            password = command[5:]
+        insock.sendall('230 Login successful.\n'.encode())
+        return user, password
+    except Exception:
+        return "", ""
--- telnet.py
+++ telnet.py
@@ -1,6 +1,9 @@
 def telnet(insock, address):
-    insock.send("Welcome to my fake Telnet server\n\nlogin: ".encode())
-    user = str(insock.recv(1024))[2:-5]
-    insock.send("password: ".encode())
-    password = str(insock.recv(1024))[2:-5]
-    return user, password
+    try:
+        insock.send("Welcome to my fake Telnet server\n\nlogin: ".encode())
+        user = str(insock.recv(1024))[2:-5]
+        insock.send("password: ".encode())
+        password = str(insock.recv(1024))[2:-5]
+        return user, password
+    except Exception:
+        return "", ""
